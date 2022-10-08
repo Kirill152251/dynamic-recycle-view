c32220d01c871209eda461b7b8ba358dbe941100
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-10-08 12:52:46 +0300

    implement delete item feature

--- app/src/main/java/com/example/dymanicrv/ItemAdapter.kt
+++ app/src/main/java/com/example/dymanicrv/ItemAdapter.kt
@@ -3,14 +3,30 @@ package com.example.dymanicrv
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dymanicrv.databinding.RvItemBinding
+import java.text.FieldPosition
 
-class ItemAdapter(private val items: List<Item>) :
+class ItemAdapter(private val data: MutableList<Item>) :
     RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val binding = RvItemBinding.bind(view)
+    private val dataList = data
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val binding = RvItemBinding.bind(view)
+        fun bind(item: Item, position: Int) {
+            binding.textNumber.text = item.value.toString()
+            binding.btnDelete.setOnClickListener {
+                deleteItem(position)
+            }
+        }
+    }
+
+    private fun deleteItem(position: Int) {
+        dataList.removeAt(position)
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,11 +40,11 @@ class ItemAdapter(private val items: List<Item>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = items[position]
-        holder.binding.textNumber.text = item.value.toString()
+        val item = dataList[position]
+        holder.bind(item, position)
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return dataList.size
     }
 }
--- app/src/main/java/com/example/dymanicrv/MainActivity.kt
+++ app/src/main/java/com/example/dymanicrv/MainActivity.kt
@@ -6,14 +6,16 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.dymanicrv.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val data = getItems(15).toMutableList()
 
         binding.recycleView.layoutManager = GridLayoutManager(this,2)
-        val adapter = ItemAdapter(getItems(15))
+        val adapter = ItemAdapter(data)
         binding.recycleView.adapter = adapter
     }
 }
